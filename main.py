0f6fc15903de9e585f53cab8b512ef9890802a05
watson-john <watson221@nevada.unr.edu>
2022-11-20 16:04:47 -0800

    initial dot and radius practice :)

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from manim import *
 
+class SecondExample(Scene):
+    def construct(self):
+        circle = Circle(radius=1, color=BLUE)
+        dot = Dot()
+        self.add(dot)
+
+        circle2 = Circle(radius=1, color=RED)
+        circle.shift(LEFT)
+        dot2 = Dot(point=LEFT)
+        self.add(dot2)
+
+        self.play(GrowFromCenter(circle))
+        self.play(GrowFromCenter(circle2))
+
+        self.wait()
